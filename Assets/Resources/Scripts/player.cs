1cdedbf482c5cce358c88dc393025c890c85deb1
rceci65 <roger.cecilia@students.salle.url.edu>
2020-03-18 19:28:00 +0100

    Moviment jugador, textura i material aplicats

---
+++ Assets/Resources/Scripts/player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+
+    float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 10.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(-moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, - moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime, 0);
+    }
+}
