96c6e8799a00c2a7de9d0323bd93c9f1677991c1
rceci65 <roger.cecilia@students.salle.url.edu>
2020-03-20 20:40:43 +0100

    Salt i seguiment de la càmera

--- Assets/Resources/Scripts/player.cs
+++ Assets/Resources/Scripts/player.cs
@@ -8,30 +8,54 @@ public class player : MonoBehaviour
 
     public Rigidbody rb;
     public float moveSpeed;
+    public float jumpForce = 2.0f;
+    public bool isGrounded;
     Vector3 movement;
+    Vector3 jump;
 
 
 
     void Start()
     {
-        moveSpeed = 10.0f;
+        moveSpeed = 5.0f;
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+        jumpForce = 30.0f;
+
     }
 
     
     void Update()
     {
         ProcessInputs();
-        //transform.Translate(-moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, - moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime, 0);
         Move();
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+        
     }
 
     private void Move(){
-        rb.velocity = new Vector3(moveSpeed * movement.x, moveSpeed * movement.y, moveSpeed * movement.z);
+        Vector3 vel = rb.velocity;
+        vel.x = moveSpeed * movement.x;
+        vel.z = moveSpeed * movement.z;
+        rb.velocity = vel;
     }
-
+    
     private void ProcessInputs()
     {
         movement = new Vector3(Input.GetAxis("Vertical"), 0.0f, - Input.GetAxis("Horizontal"));
 
+
+    }
+    
+    
+    private void OnCollisionStay()
+    {
+        isGrounded = true;
     }
 }
