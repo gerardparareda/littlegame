dc863c7a3994ea4af379a6cdf8829eda604a8f3e
Franquesa-Balcells <adria.franquesa.2015@salleurl.edu>
2020-04-13 18:23:50 +0200

    Commit pre roger

---
+++ Assets/Resources/Scripts/Enemies/EnemyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float lookRadious = 10f;
+    // Start is called before the first frame update
+
+    Transform target;
+    NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmosSelected ()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadious);
+    }
+}
