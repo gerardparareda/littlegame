3a4c77a17aa2ddc96e39db574c91fbc078770d86
Franquesa-Balcells <adria.franquesa.2015@salleurl.edu>
2020-05-12 17:02:25 +0200

    combat i nau

---
+++ Assets/Resources/Scripts/LivesUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivesUI : MonoBehaviour
+{
+
+    public static LivesUI Instance { get; set; }
+
+    public GameObject heartPanel;
+    public GameObject heart;
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        if (Instance == null) { Instance = this; }
+        else { Destroy(gameObject); }
+    }
+
+    public void AddHeart()
+    {
+        GameObject cor = Instantiate(heart, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+        cor.transform.SetParent(heartPanel.transform);
+        /*this.title = title;
+        this.description = description;*/
+        //CreateQuest();
+    }
+
+    public void RemoveHeart()
+    {
+        GameObject child = heartPanel.transform.GetChild(0).gameObject;
+        Destroy(child);
+
+    }
+    // Update is called once per frame
+   
+}
---
+++ Assets/Resources/Scripts/LookAtCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    private GameObject Target = null;
+
+    private Vector3 TargetPosition;
+
+    void Start()
+    {
+        if (Target == null)
+        {
+            Target = Camera.main.gameObject;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (TargetPosition != Target.transform.position)
+        {
+            TargetPosition = Target.transform.position;
+            transform.LookAt(TargetPosition);
+        }
+    }
+}
