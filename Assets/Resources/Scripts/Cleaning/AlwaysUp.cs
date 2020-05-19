c755d56056308391c53b8e7fe13d2bf9e363ee89
Gerard Parareda Gallifa <gerard.parareda@students.salle.url.edu>
2020-05-19 22:17:59 +0200

    Added brokable boxes

---
+++ Assets/Resources/Scripts/Cleaning/AlwaysUp.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlwaysUp : MonoBehaviour
+{
+    void Update()
+    {
+        var rotation = Quaternion.LookRotation(Vector3.up, Vector3.forward);
+        transform.rotation = rotation;
+    }
+}
