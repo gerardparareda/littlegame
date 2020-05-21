3f2c8e8afb6410b048e446ee442d062a56118a7e
Gerard Parareda Gallifa <gerard.parareda@students.salle.url.edu>
2020-05-22 01:02:56 +0200

    Fixes i part 3

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -92,6 +92,7 @@ public class PlayerController : MonoBehaviour
                     {
                         interactable.Hit();
                     }
+
                 }
             }
         }
